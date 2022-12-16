d9798fd414815ab6d0c40f23952078b96cca7bf3
sadam <sadamccr@gmail.com>
2022-12-17 00:13:57 +0500

    js updated for validation

--- js/script.js
+++ js/script.js
@@ -1,36 +1,44 @@
 function validate(){
     let username=document.getElementById('username')
     let password =document.getElementById('password')
+    let status =document.getElementById('status')
+    
 
     console.log(username)
     console.log(password)
 
     if(username.value!=''){
         if(username.value.length < 4){
-            alert("Username should be longer than 4 letters.")
+            // alert("Username should be longer than 4 letters.")
+            status.innerHTML='Username should be longer than 4 letters.'
+            return;
         }
         if(username.value.length > 10){
-            alert("Username should be less than 10 letters.")
+            status.innerHTML = 'Username should be less than 10 letters.'
             return;
         }
     }
     else{
-        alert('Please enter a name')
+        status.innerHTML = 'Please enter a name.'
+        return;
     }
 
     if(password.value!=''){
         if(password.value.length < 4){
-            alert("Password should be longer than 4 letters.")
+            status.innerHTML = "Password should be longer than 4 letters."
             return;
         }
         if(password.value.length > 10){
-            alert("Password should be less than 10 letters.")
+            status.innerHTML = "Password should be less than 10 letters."
             return;
         }
     }
     else{
-        alert('Please enter passowrd')
+        status.innerHTML = 'Please enter passowrd'
         return;
     }
+
+    status.innerHTML='Validated!'
+    status.style.color = 'green'
 
 }
