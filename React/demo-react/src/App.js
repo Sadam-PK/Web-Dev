0967fbeede67ffafd7596b5e57c766c3d72d5f66
sadam <sadamccr@gmail.com>
2022-12-20 12:05:02 +0500

    context file added, and intro done

--- React/demo-react/src/App.js
+++ React/demo-react/src/App.js
@@ -2,12 +2,19 @@ import './App.css';
 import './components/functionalComponents'
 import FunctionalComponent from './components/functionalComponents';
 import React, { useState } from 'react';
+import { UserInfo } from './AppContext';
+
+
 function App() {
+  let [userName, setUsername] = useState('To Al-Nafi')
   return (
     <div className="App">
-      <header className="App-header">
-        <FunctionalComponent/>
-      </header>
+      <h1>Main App File</h1>
+      <UserInfo.Provider value={[userName, setUsername]}>
+        <header className="App-header">
+          <FunctionalComponent />
+        </header>
+      </UserInfo.Provider>
     </div>
   );
 }
--- React/demo-react/src/components/functionalComponents.js
+++ React/demo-react/src/components/functionalComponents.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
-
+import React, { useState, useContext } from 'react'
+import {UserInfo} from '../AppContext'
 
 function FunctionalComponent() {
+    let [userName, setUsername] = useContext(UserInfo)
     function increment() {
         setValue(getValue + 1)
     }
@@ -12,9 +13,10 @@ function FunctionalComponent() {
     let [getValue, setValue] = useState(10);
     return (
         <>
-            <h1>Welcome on-board {getValue}</h1>
+            <h1>Welcome on-board {userName} {getValue}</h1>
             <button onClick={increment}>Increment</button>
             <button onClick={decrement}>Decrement</button>
+            <button onClick={()=>{setUsername('Nafi World..')}}>CHANGE</button>
         </>
     )
 }
